e5b064252604c9eb51952fd07d75b8f4fc838489
Ahmed <48266030+Ahmed-797@users.noreply.github.com>
2019-03-06 09:07:32 +0530

    Update portugese_bank_campaign.py

--- portugese_bank_campaign.py
+++ portugese_bank_campaign.py
@@ -1,8 +1,9 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd #importing,handling data
+import matplotlib.pyplot as plt #plotting
 
 data = pd.read_csv('bank-additional-full.csv', sep = ';')
 
+#Dependent variable
 y = data['y']
 y = y.replace(['yes','no'],(1,0))
 
@@ -17,18 +18,23 @@ def encode_features(df_train):
         df_train[feature] = le.transform(df_train[feature])
     return df_train
 
+#Encoding non-numerical values
 data = encode_features(data)
 
+#Independent variables
 X = data.drop(['y'], axis = 1)
 
+#Splitting data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=100)
 
+#Using RandomForest Classifier
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(n_estimators = 100, max_depth=10, random_state=100,criterion='gini')
 clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 
+#Calculating accuracy of the prediction
 from sklearn.metrics import accuracy_score
 accuracy_score(y_pred,y_test)
 
