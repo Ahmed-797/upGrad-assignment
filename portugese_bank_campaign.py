e26ee69acc5410dc20ca73c58cda6d00ac7911a0
Ahmed <48266030+Ahmed-797@users.noreply.github.com>
2019-03-06 09:01:50 +0530

    Create portugese_bank_campaign.py

---
+++ portugese_bank_campaign.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('bank-additional-full.csv', sep = ';')
+
+y = data['y']
+y = y.replace(['yes','no'],(1,0))
+
+from sklearn import preprocessing
+
+def encode_features(df_train):
+    features = ['job','marital','default','education','housing','loan','contact','month','day_of_week','poutcome']
+    
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df_train[feature])
+        df_train[feature] = le.transform(df_train[feature])
+    return df_train
+
+data = encode_features(data)
+
+X = data.drop(['y'], axis = 1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=100)
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators = 100, max_depth=10, random_state=100,criterion='gini')
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_pred,y_test)
+
+#plotting importance of all factors
+importances=pd.Series(clf.feature_importances_, index=X.columns)
+importances.plot(kind='barh', figsize=(14,8))
